91cc67f8e9c580dd6df6dcbab9c1f470c04c01de
Huang <huangwenfuture@sina.com>
2019-03-14 08:00:33 +0100

    mavlink_main: remove some mavlink stream which is not needed
    Signed-off-by: Huang <huangwenfuture@sina.com>

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -2046,40 +2046,20 @@ Mavlink::task_main(int argc, char *argv[])
 
 	case MAVLINK_MODE_ONBOARD:
 		configure_stream("ACTUATOR_CONTROL_TARGET0", 10.0f);
-		configure_stream("ADSB_VEHICLE");
-		configure_stream("ALTITUDE", 10.0f);
 		configure_stream("ATTITUDE", 100.0f);
-		configure_stream("ATTITUDE_QUATERNION", 50.0f);
-		configure_stream("ATTITUDE_TARGET", 10.0f);
-		configure_stream("CAMERA_CAPTURE", 2.0f);
-		configure_stream("CAMERA_IMAGE_CAPTURED");
-		configure_stream("CAMERA_TRIGGER");
-		configure_stream("COLLISION");
-		configure_stream("DEBUG", 10.0f);
-		configure_stream("DEBUG_VECT", 10.0f);
-		configure_stream("DISTANCE_SENSOR", 10.0f);
+		configure_stream("ATTITUDE_TARGET", 30.0f);
 		configure_stream("ESTIMATOR_STATUS", 1.0f);
 		configure_stream("EXTENDED_SYS_STATE", 5.0f);
-		configure_stream("GLOBAL_POSITION_INT", 50.0f);
-		configure_stream("GPS_RAW_INT");
 		configure_stream("HIGHRES_IMU", 50.0f);
 		configure_stream("HOME_POSITION", 0.5f);
 		configure_stream("LOCAL_POSITION_NED", 30.0f);
-		configure_stream("NAMED_VALUE_FLOAT", 10.0f);
-		configure_stream("NAV_CONTROLLER_OUTPUT", 10.0f);
-		configure_stream("OPTICAL_FLOW_RAD", 10.0f);
 		configure_stream("PING", 1.0f);
-		configure_stream("POSITION_TARGET_GLOBAL_INT", 10.0f);
-		configure_stream("POSITION_TARGET_LOCAL_NED", 10.0f);
-		configure_stream("RC_CHANNELS", 20.0f);
-		configure_stream("SCALED_IMU", 50.0f);
-		configure_stream("SERVO_OUTPUT_RAW_0", 10.0f);
+		configure_stream("POSITION_TARGET_LOCAL_NED", 30.0f);
+		configure_stream("RC_CHANNELS", 10.0f);
 		configure_stream("SYS_STATUS", 5.0f);
 		configure_stream("SYSTEM_TIME", 1.0f);
 		configure_stream("TIMESYNC", 10.0f);
-		configure_stream("VFR_HUD", 10.0f);
 		configure_stream("VISION_POSITION_ESTIMATE", 10.0f);
-		configure_stream("WIND_COV", 10.0f);
 		break;
 
 	case MAVLINK_MODE_OSD:
